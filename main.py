70e256f5d7572aaf9e3f42e077c948324c0930f4
Yuki Ito <acomagu@gmail.com>
2016-08-31 10:09:15 -0700

    add record when push button

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import totext
 import RPi.GPIO as gpio
 
 BUTTONPIN = 12
+MICDEVICE = 2
 gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 gpio.setup(BUTTONPIN, gpio.IN)
@@ -21,11 +22,13 @@ def wait_until_push_button():
 
 if __name__ == '__main__':
     while True:
+        rec = record.Recorder(device=MICDEVICE, nChannel=1, framerate=44100, framesPerBuffer=4096)
         wait_until_push_button()
         print("PUSHED")
+        rec.start()
         wait_until_release_button()
         print("RELEASED")
-    #
-    # rec = record.Recorder(device=2, nChannel=1, framerate=44100, framesPerBuffer=4096)
-    # text = totext.main(audio, framerate=44100)
-    # print(text)
+        rec.end()
+        audio = rec.close()
+        text = totext.main(audio, framerate=44100)
+        print(text)
