d049f465541aa0067fffcd9e87aa69bec70757d0
Yuki Ito <acomagu@gmail.com>
2016-08-31 08:47:02 -0700

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import record
 import totext
+import RPi.GPIO as gpio
+
+BUTTONPIN = 12
+gpio.setwarnings(False)
+gpio.setmode(gpio.BCM)
+gpio.setup(BUTTONPIN, gpio.IN)
+
+def is_button_pressed():
+    return gpio.input(BUTTONPIN)
+
+def wait_until_release_button():
+    while is_button_pressed():
+        pass
+
+def wait_until_push_button():
+    while not is_button_pressed():
+        pass
 
 if __name__ == '__main__':
-    rec = record.Recorder(device=2, nChannel=1, framerate=44100, framesPerBuffer=4096)
-    text = totext.main(audio, framerate=44100)
-    print(text)
+    while True:
+        wait_until_push_button()
+        print("PUSHED")
+        wait_until_release_button()
+        print("RELEASED")
+    #
+    # rec = record.Recorder(device=2, nChannel=1, framerate=44100, framesPerBuffer=4096)
+    # text = totext.main(audio, framerate=44100)
+    # print(text)
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-env GOOGLE_APPLICATION_CREDENTIALS=./smart-bookmark-66ef9381f983.json python3 main.py
+env GOOGLE_APPLICATION_CREDENTIALS=./key/smart-bookmark-66ef9381f983.json python3 main.py
